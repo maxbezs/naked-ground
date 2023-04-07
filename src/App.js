f418d476430e43a22017b5b3269925fe8da33438
maxbezs <baks.bezsmertnuy@gmail.com>
2023-04-07 08:59:28 +0200

    1

--- src/App.js
+++ src/App.js
@@ -151,7 +151,7 @@ GROUND is a daily habit which brings positivity and company to everyone's day"</
           <section  className='main-div2-section'>
             <div className='main-div2-section-div'>
               <div className='main-div2-section-div-div'>
-                <div style={{height: "256.984px"}}>
+                <div style={{height: "256.984px", display: "flex", justifyContent:"center"}}>
                   <img src={image34} alt=""/>
                 </div>
               </div>
@@ -345,8 +345,8 @@ GROUND is a daily habit which brings positivity and company to everyone's day"</
                   <li className='footer-div-div-div1-ul-li'>
                     <a className='footer-div-div-div1-ul-li-a' href="https://www.tiktok.com/@nakedground.es" target="_blank" rel="noopener noreferrer" aria-label="Pinterest">
                       <span>
-                        <svg style={{color: "white", verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"> 
-                          <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z" fill="white"></path> 
+                        <svg style={{color: "#000", verticalAlign: "middle"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"> 
+                          <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z" fill="black"></path> 
                         </svg>
                       </span>
                     </a>
